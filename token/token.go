4758e6bcc82b525641c2e741937f805fd780112a
HemanthBangera <hemanthbangera843@gmail.com>
2025-09-27 00:30:06 +0530

    Implemented basic lexer

--- token/token.go
+++ token/token.go
@@ -25,5 +25,18 @@ const (
  RBRACE = "}"
  // Keywords
  FUNCTION = "FUNCTION"
- LET= "LET"
+ LET = "LET"
  )
+
+
+ var keywords = map[string]TokenType{
+     "fn":FUNCTION,
+     "let":LET,
+ }
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok{
+        return tok
+    }
+    return IDENT
+}
