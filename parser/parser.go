f3d1fb80ce6fefac3fc0303d520bcabe59f55f92
HemanthBangera <hemanthbangera843@gmail.com>
2025-09-30 00:10:27 +0530

    Implemented the let statment parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+    "fmt"
     "github.com/HemanthBangera/PicoLang/ast"
     "github.com/HemanthBangera/PicoLang/lexer"
     "github.com/HemanthBangera/PicoLang/token"
@@ -10,10 +11,14 @@ type Parser struct {
     l *lexer.Lexer
     curToken token.Token
     peekToken token.Token
+    errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-    p := &Parser{l: l}
+    p := &Parser{
+        l: l,
+        errors: []string{},
+    }
 
     p.nextToken()
     p.nextToken()
@@ -24,13 +29,13 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
     p.curToken = p.peekToken
     p.peekToken = p.l.NextToken()
-}
+} 
 
 func (p *Parser) ParseProgram() *ast.Program {
     program := &ast.Program{}
     program.Statements = []ast.Statement{}
 
-    for p.curToken.Type != token.EOF {
+    for !p.curTokenIs(token.EOF) {
         stmt := p.parseStatement()
         if stmt != nil {
             program.Statements = append(program.Statements,stmt)
@@ -85,7 +90,16 @@ func(p *Parser) expectPeek(t token.TokenType) bool {
         p.nextToken()
         return true
     } else {
+        p.peekError(t)
         return false
     }
 }
 
+func(p *Parser) Errors() []string {
+    return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+    msg := fmt.Sprintf("expected next token to be %s, got %s instead", t,p.peekToken.Type)
+    p.errors = append(p.errors,msg)
+}
