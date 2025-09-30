4dec76652fd6ae06911be61667a56e530432cf48
HemanthBangera <hemanthbangera843@gmail.com>
2025-09-30 15:08:01 +0530

    AST has been prepared for implementing the pratt parser

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "github.com/HemanthBangera/PicoLang/token"
+import (
+    "github.com/HemanthBangera/PicoLang/token"
+    "bytes"
+)
 
 type Node interface {
     TokenLiteral() string
+    String() string
 }
 
 type Statement interface {
@@ -21,6 +25,11 @@ type ReturnStatement struct {
     ReturnValue Expression
 }
 
+type ExpressionStatement struct {
+    Token token.Token
+    Expression Expression
+}
+
 type Program struct {
     Statements []Statement
 }
@@ -31,6 +40,16 @@ func (p *Program) TokenLiteral() string {
     } else {
         return ""
     }
+}
+
+func (p *Program) String() string {
+    var out bytes.Buffer
+
+    for _, s := range p.Statements {
+        out.WriteString(s.String())
+    }
+    
+    return out.String()
 }
 
 type LetStatement struct {
@@ -44,6 +63,21 @@ func (ls *LetStatement) TokenLiteral() string {
     return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+    return out.String()
+}
+
 type Identifier struct {
     Token token.Token
     Value string
@@ -54,7 +88,34 @@ func (i *Identifier) TokenLiteral() string {
     return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+    return i.Value;
+}
+
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
+func (rs *ReturnStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(rs.TokenLiteral() + " ")
+
+    if rs.ReturnValue != nil {
+        out.WriteString(rs.ReturnValue.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+    return ""
+}
 
