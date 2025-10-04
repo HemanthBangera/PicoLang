cd5d8110eaacb80d570cc7420fa74a9da48faaea
HemanthBangera <hemanthbangera843@gmail.com>
2025-10-04 15:25:39 +0530

    Handled errors

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ const (
     BOOLEAN_OBJ = "BOOLEAN"
     NULL_OBJ = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
+    ERROR_OBJ = "ERROR"
 )
 
 type ObjectType string
@@ -54,5 +55,12 @@ type ReturnValue struct{
 func (ev *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 
 func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+type Error struct {
+    Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string { return "ERROR: "+e.Message }
 
 
