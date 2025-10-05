a96b02ba9ddb60eeb6c4a032b7753c3f705f6976
HemanthBangera <hemanthbangera843@gmail.com>
2025-10-05 22:01:52 +0530

    Implemented the logic for supporting functions, higher-order functions an d closures

--- object/object.go
+++ object/object.go
@@ -2,6 +2,9 @@ package object
 
 import (
     "fmt"
+    "bytes"
+    "strings"
+    "github.com/HemanthBangera/PicoLang/ast"
 )
 
 const (
@@ -10,6 +13,7 @@ const (
     NULL_OBJ = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
     ERROR_OBJ = "ERROR"
+    FUNCTION_OBJ = "FUNCTION"
 )
 
 type ObjectType string
@@ -63,20 +67,28 @@ type Error struct {
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string { return "ERROR: "+e.Message }
 
-func NewEnvironment() *Environment {
-    s := make(map[string]Object)
-    return &Environment{store: s}
+
+ type Function struct {
+     Parameters []*ast.Identifier
+     Body *ast.BlockStatement
+     Env *Environment
  }
 
-type Environment struct {
-    store map[string]Object
- }
+ func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+ func (f *Function) Inspect() string {
+     var out bytes.Buffer
 
-func (e *Environment) Get(name string) (Object, bool) {
-    obj, ok := e.store[name]
-    return obj, ok
-}
-func (e *Environment) Set(name string, val Object) Object {
-    e.store[name] = val
-    return val
+     params := []string{}
+     for _, p := range f.Parameters {
+         params = append(params,p.String())
+     }
+
+     out.WriteString("fn")
+     out.WriteString("(")
+     out.WriteString(strings.Join(params,", "))
+     out.WriteString(") {\n")
+     out.WriteString(f.Body.String())
+     out.WriteString("\n}")
+
+     return out.String()
  }
