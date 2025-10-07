18098bb5408d18146b740fb8e4c974c8509aeafd
HemanthBangera <hemanthbangera843@gmail.com>
2025-10-07 16:42:42 +0530

    Integrated puts to print the give arguments

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,7 +1,8 @@
 package evaluator
 
 import (
-    "github.com/HemanthBangera/PicoLang/object"   
+    "github.com/HemanthBangera/PicoLang/object"  
+    "fmt"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -89,6 +90,16 @@ var builtins = map[string]*object.Builtin{
             copy(newElements, arr.Elements)
             newElements[length] = args[1]
             return &object.Array{Elements: newElements}
+        },
+    },
+
+    "puts": &object.Builtin{
+        Fn :func(args ...object.Object) object.Object {
+            for _, arg := range args {
+                fmt.Println(arg.Inspect())
+            }
+
+            return NULL
         },
     },
  }
